a73ad41eff0dbc62ec972f28f6c5604d63b028da
Alexey Serebryakov <silwar40k@gmail.com>
2019-02-13 18:26:29 +0300

    сделал проверки открытия файлов, проверку количества аргументов, вывод в фаил по строчно

--- lw1/CopyFile/CopyFile.cpp
+++ lw1/CopyFile/CopyFile.cpp
@@ -7,21 +7,43 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-
-	if (argc < 2)
+	if (argc != 3)
 	{
 		cout << "Invalid arguments count\n"
 			 << "Usage: copyfile.exe <input file> <output file>\n";
 		return 1;
 	}
 
-	string str = "";
 	ifstream input(argv[1]);
+
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+	}
+
+	ofstream output(argv[2]);
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+
+	string str = "";
 
 	while (getline(input, str))
 	{
-		cout << str;
+		output << str;
 	}
+
+	if (!output.flush())
+	{
+		cout << "Failed to save data on disk\n";
+		return 1;
+	}
+
+	input.close();
+	output.close();
 
 	return 0;
 }
--- lw1/CopyFile/Test.bat
+++ lw1/CopyFile/Test.bat
@@ -1,8 +1,11 @@
 set PROGRAM="%~1"
-set OUT="%TEMP%\out.txt"
+set OUT="out.txt"
 
-%PROGRAM% Hello.txt > OUT || goto err
+%PROGRAM% Hello.txt OUT || goto err
 fc OUT Hello-out.txt || goto err
+
+%PROGRAM% Empty.txt OUT || goto err
+fc OUT Empty-out.txt || goto err
 
 echo Program testing succeeded
 exit 0

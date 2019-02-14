931375154f9a59eceefe811155a8bc953ba29c76
Alexey Serebryakov <silwar40k@gmail.com>
2019-02-14 22:04:18 +0300

    calcbits добавил проверку количества аргуметов

--- lw1/calcbits/Test.bat
+++ lw1/calcbits/Test.bat
@@ -3,6 +3,11 @@ set PROGRAM="%~1"
 %PROGRAM% 5 > "%TEMP%\output.txt" || goto err
 fc "%TEMP%\output.txt" five-out.txt || goto err
 
+:: Проверка количества аргументов. Если не соответствует 2-м, то выводит сообщение об ошибке
+%PROGRAM% > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto err
+fc "%TEMP%\output.txt" expected-output-when-number-of-arguments-is-not-2.txt || goto err
+
 echo Program testing succeeded
 exit 0
 

2bb0c2fc1397ca9447a11c6426193c77fb14eeb0
Alexey Serebryakov <silwar40k@gmail.com>
2019-06-01 15:04:34 +0300

    Тесты для сокращённых форм сложения, вычитания векторов и умножения и деления вектора на скаляр

--- lw5/Vector3DTest/Test.cpp
+++ lw5/Vector3DTest/Test.cpp
@@ -81,3 +81,49 @@ TEST_CASE("3D vector division by scalar, [CVector3D]")
 	CHECK(IsEequal(resultVector.m_y, 1.7));
 	CHECK(IsEequal(resultVector.m_z, 2.8));
 }
+
+TEST_CASE("Increasing the length of the 3D vector, [CVector3D]")
+{
+	CVector3D vector1(1.2, 3.4, 5.6);
+	CVector3D vector2(9.8, 7.6, 4.3);
+
+	vector1 += vector2;
+
+	CHECK(IsEequal(vector1.m_x, 11));
+	CHECK(IsEequal(vector1.m_y, 11));
+	CHECK(IsEequal(vector1.m_z, 9.9));
+}
+
+TEST_CASE("3D vector length reduction, [CVector3D]")
+{
+	CVector3D vector1(1.2, 3.4, 5.6);
+	CVector3D vector2(9.8, 7.6, 4.3);
+
+	vector1 -= vector2;
+
+	CHECK(IsEequal(vector1.m_x, 8.6));
+	CHECK(IsEequal(vector1.m_y, 4.2));
+	CHECK(IsEequal(vector1.m_z, -1.3));
+}
+
+TEST_CASE("Short form of 3D vector multiplication by scalar, [CVector3D]")
+{
+	CVector3D vector(1.2, 3.4, 5.6);
+
+	vector *= 3.3;
+
+	CHECK(IsEequal(resultVector.m_x, 3.96));
+	CHECK(IsEequal(resultVector.m_y, 11.22));
+	CHECK(IsEequal(resultVector.m_z, 18.48));
+}
+
+TEST_CASE("Short form of 3D vector division by scalar, [CVector3D]")
+{
+	CVector3D vector(1.2, 3.4, 5.6);
+
+	vector /= 2;
+
+	CHECK(IsEequal(resultVector.m_x, 0.6));
+	CHECK(IsEequal(resultVector.m_y, 1.7));
+	CHECK(IsEequal(resultVector.m_z, 2.8));
+}

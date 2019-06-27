67607e555d39198e207629c34cbc4464ea6e5b79
Alexey Serebryakov <silwar40k@gmail.com>
2019-06-27 06:53:17 +0300

    Mylist реализован, но тесты не проходят

--- lw7/MyListTest/Tests.cpp
+++ lw7/MyListTest/Tests.cpp
@@ -1,1 +1,35 @@
 #include "pch.h"
+#include "lw7/MyList/CMyList.h"
+
+using namespace std;
+
+SCENARIO("New custom list")
+{
+	GIVEN("New List of int")
+	{
+		CMyList<int> list;
+		int item1 = 1, item2 = 2;
+
+		REQUIRE(list.GetSize() == 0);
+
+		WHEN("Insert one item at the begining of the list")
+		{
+			list.InsertAtTheBegining(item1);
+
+			THEN("The number of items in the list increases by 1")
+			{
+				CHECK(list.GetSize() == 1);
+			}
+		}
+
+		WHEN("Insert one item at the begining of the list")
+		{
+			list.InsertToTheEnd(item2);
+
+			THEN("The number of items in the list increases by 1")
+			{
+				CHECK(list.GetSize() == 2);
+			}
+		}
+	}
+}

83083c8fc91e292e433b26ef3867b798d81db4b2
Alexey Serebryakov <silwar40k@gmail.com>
2019-07-11 06:40:14 +0300

    Инициализация стека

---
+++ lw7/MyStack/CMyStack.h
@@ -0,0 +1,66 @@
+#pragma once
+
+template <typename T>
+class CMyStack
+{
+	struct Item
+	{
+		Item(T data, Item* next)
+			: data(std::move(data))
+			, next(next)
+		{
+		}
+
+		T data;
+		Item* next;
+	};
+
+public:
+	CMyStack() = default;
+
+	CMyStack(CMyStack const& stack)
+		: CMyStack()
+	{
+		try
+		{
+			Item* stackPtr = stack.m_top;
+
+			while (stackPtr)
+			{
+				Item* newItem = new Item(stack->data, nullptr);
+
+				if (this->m_top)
+				{
+					newItem->next = this->m_top
+				}
+
+				this->m_top = newItem;
+				stackPtr = stackPtr->next;
+			}
+		}
+		catch (...)
+		{
+			Clear();
+			throw;
+		}
+	}
+
+	void Clear()
+	{
+		while (m_top)
+		{
+			Item* itemPtr = m_top;
+			m_top = m_top->next;
+			delete itemPtr;
+		}
+	}
+
+	~CMyStack()
+	{
+		Clear();
+		free(m_top);
+	}
+
+private:
+	Item* m_top = nullptr;
+};
---
+++ lw7/MyStackTest/Tests.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "lw7/MyStack/CMyStack.h"

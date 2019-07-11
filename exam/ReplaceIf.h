2af87de21728dcd1833eb6640889fbd43821f426
Alexey Serebryakov <silwar40k@gmail.com>
2019-07-12 01:00:30 +0300

    Правка

--- exam/ReplaceIf.h
+++ exam/ReplaceIf.h
@@ -5,7 +5,7 @@
 template <typename it, typename T, typename Predicate>
 void ReplaceIf(it first, it last, Predicate predicate, const T& newValue)
 {
-	for (auto i = first; i != last; ++i)
+	for (it i = first; i != last; ++i)
 	{
 		if (predicate(*i))
 		{

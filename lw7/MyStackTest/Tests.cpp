5923c9424bfd1149c56f891cb679fa317126980c
Alexey Serebryakov <silwar40k@gmail.com>
2019-07-11 19:12:24 +0300

    Тесты для строкового типа

--- lw7/MyStackTest/Tests.cpp
+++ lw7/MyStackTest/Tests.cpp
@@ -153,3 +153,156 @@ TEST_CASE("Stack of integer")
 		}
 	}
 }
+
+TEST_CASE("Stack of string")
+{
+	GIVEN("stack")
+	{
+		CMyStack<std::string> stack;
+		std::string testValue = "Hello";
+		std::string value2 = "world!";
+		std::string reciever;
+
+		WHEN("Ñhecking if the stack is empty")
+		{
+			THEN("The result is positive")
+			{
+				CHECK(stack.IsEmpty());
+			}
+		}
+
+		WHEN("Push value to stack")
+		{
+			stack.Push(testValue);
+
+			THEN("Stack is not empty")
+			{
+				CHECK(!stack.IsEmpty());
+			}
+		}
+
+		WHEN("Pushing out from stack with 1 value")
+		{
+			stack.Push(testValue);
+			stack.Pop();
+
+			THEN("Stack is empty")
+			{
+				CHECK(stack.IsEmpty());
+			}
+		}
+
+		WHEN("Pushing out from empty stack")
+		{
+			THEN("Get error message")
+			{
+				CHECK_THROWS_WITH(stack.Pop(), "ERROR: Stack is empty\n");
+			}
+		}
+
+		WHEN("Get value from not empty stack")
+		{
+			stack.Push(testValue);
+			reciever = stack.GetTop();
+
+			THEN("Stack is empty")
+			{
+				CHECK(reciever == testValue);
+			}
+		}
+
+		WHEN("Get value from empty stack")
+		{
+			THEN("Get error message")
+			{
+				CHECK_THROWS_WITH(reciever = stack.GetTop(), "ERROR: Stack is empty\n");
+			}
+		}
+
+		WHEN("Clear not empty stack")
+		{
+			stack.Push(testValue);
+			stack.Push(testValue);
+			stack.Clear();
+
+			THEN("Stack is empty")
+			{
+				CHECK(stack.IsEmpty());
+			}
+		}
+
+		WHEN("Ñreate a new stack copy of the old")
+		{
+			stack.Push(testValue);
+			stack.Push(value2);
+			CMyStack<std::string> newStack(stack);
+
+			THEN("Their elements coincide")
+			{
+				CHECK(stack.GetTop() == newStack.GetTop());
+				stack.Pop();
+				newStack.Pop();
+				CHECK(stack.GetTop() == newStack.GetTop());
+			}
+		}
+
+		WHEN("Using copy operator = ")
+		{
+			stack.Push(testValue);
+			stack.Push(value2);
+			CMyStack<std::string> newStack = stack;
+
+			THEN("New stack are equal old stack")
+			{
+				CHECK(stack.GetTop() == newStack.GetTop());
+				stack.Pop();
+				newStack.Pop();
+				CHECK(stack.GetTop() == newStack.GetTop());
+				stack.Pop();
+				newStack.Pop();
+				CHECK(stack.IsEmpty());
+				CHECK(newStack.IsEmpty());
+			}
+		}
+
+		WHEN("Ñreate a new stack using move constructor")
+		{
+			stack.Push(testValue);
+			stack.Push(value2);
+			CMyStack<std::string> dublicate = stack;
+			CMyStack<std::string> newStack(std::move(stack));
+
+			THEN("Their elements coincide")
+			{
+				CHECK(dublicate.GetTop() == newStack.GetTop());
+				dublicate.Pop();
+				newStack.Pop();
+				CHECK(dublicate.GetTop() == newStack.GetTop());
+				dublicate.Pop();
+				newStack.Pop();
+				CHECK(dublicate.IsEmpty());
+				CHECK(newStack.IsEmpty());
+			}
+		}
+
+		WHEN("Using moving operator = ")
+		{
+			stack.Push(testValue);
+			stack.Push(value2);
+			CMyStack<std::string> dublicate = stack;
+			CMyStack<std::string> newStack = std::move(stack);
+
+			THEN("New stack are equal old stack")
+			{
+				CHECK(dublicate.GetTop() == newStack.GetTop());
+				dublicate.Pop();
+				newStack.Pop();
+				CHECK(dublicate.GetTop() == newStack.GetTop());
+				dublicate.Pop();
+				newStack.Pop();
+				CHECK(dublicate.IsEmpty());
+				CHECK(newStack.IsEmpty());
+			}
+		}
+	}
+}
